9a8834100ae80a1ba8eb1a4ce1bab1d427e6f4c3
이수현 <164829006+JustinLee02@users.noreply.github.com>
2026-04-28 14:17:49 +0900

    Implement main function to find combinations of values

---
+++ p1/main.c
@@ -0,0 +1,25 @@
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+    
+    int found = 0;
+    
+    for (int a=1; a*900 <= N; a++) {
+        for (int b=2; a * 900 + b * 750 <= N; b+=2) {
+            for (int c=1; a*900 + b*750 + c*200 <=N; c++) {
+                if (a*900 + b*750 + c*200 ==N) {
+                    if (c<a || c < b) {
+                    printf("%d %d %d\n", a, b, c);
+                    found = 1;
+                    }
+                }
+            }
+        }
+    }
+    if (!found) {
+        printf("none\n");
+    }
+
+    return 0;
+}
